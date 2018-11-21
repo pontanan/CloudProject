9f4b8f6b22f19abde377b017609ec0ac9a260b8e
Pontus Anderö <datapontus@hotmail.com>
2018-11-21 16:26:15 +0100

    added back app.js

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-var express = require('express');
-var app = express();
-const sql = require('mssql');
+hejsan
 
-var config = {
-    user: 'CityForumDBAdmin',
-    password: 'cloudService123',
-    server: 'cityforumdatabase.c7xajpqu6x8l.eu-west-1.rds.amazonaws.com', 
-    database: 'cityforumDB' 
-};
 
-app.get('/login', function(req, res) 
-{
-    // Connect to database
-    sql.connect(config, function (err) 
-    {
-    
-        if (err) console.log(err);
 
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from Account', function (err, recordset) 
-        {
-            
-            if (err) console.log(err)
+hejhejhejee
+hejehejeje
 
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
 
-});
+d
+fsf
+SourceBufferListfsfsfsfsfsfsf

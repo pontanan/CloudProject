1a0df19fec205e0e3f99d121b465602da7ceac52
Pontus Anderö <datapontus@hotmail.com>
2018-11-27 16:26:39 +0100

    Standardisation

--- application.js
+++ application.js
@@ -1,74 +1,64 @@
-var db = require('./db');
-var express = require('express');
-var fs = require('fs');
-const {google} = require('googleapis');
+var db = require('./db')
+var express = require('express')
+var fs = require('fs')
+const { google } = require('googleapis')
 
-var app = express();
+var app = express()
 
-var authContent = fs.readFileSync("authorisation/client_auth.json");
-var jsonAuthContent = JSON.parse(authContent);
+var authContent = fs.readFileSync('authorisation/client_auth.json')
+var jsonAuthContent = JSON.parse(authContent)
 
 const oauth2Client = new google.auth.OAuth2(
-    jsonAuthContent.web.client_id,
-    jsonAuthContent.web.client_secret,
-    jsonAuthContent.web.redirect_uris
-);
+  jsonAuthContent.web.client_id,
+  jsonAuthContent.web.client_secret,
+  jsonAuthContent.web.redirect_uris
+)
 
 const scopes = [
-    'https://www.googleapis.com/auth/plus.me',
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile'
-];
+  'https://www.googleapis.com/auth/plus.me',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile'
+]
 
-const url = oauth2Client.generateAuthUrl(
-{
-    access_type: 'offline',
-    scope: scopes
-});
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: scopes
+})
 
-app.get('/', function(req, res) 
-{
-    res.redirect(301, url)
-    res.end();
-});
+app.get('/', function (req, res) {
+  res.redirect(301, url)
+  res.end()
+})
 
-app.get('/auth', function(req, res) 
-{
-    var authCode = req.query.code;
-    var error = req.query.error;
-    
-    if(authCode != null)
-    {
-        const {tokens} = oauth2Client.getToken(authCode);
-        oauth2Client.setCredentials(tokens);
-    }
-    else{ console.log(error); }
+app.get('/auth', function (req, res) {
+  var authCode = req.query.code
+  var error = req.query.error
 
-    res.redirect(301, '/Account');
-    res.end();
-});
+  if (authCode != null) {
+    const { tokens } = oauth2Client.getToken(authCode)
+    oauth2Client.setCredentials(tokens)
+  } else { console.log(error) }
 
-app.get('/Account', function(req, res)
-{
-    var request = new db.Request();
-           
-    // query to the database and get the records
-    request.query('select * from Account', function (err, result) 
-    {
-        if (err) {console.log(err); res.send(err.message);}
+  res.redirect(301, '/Account')
+  res.end()
+})
 
-        // send records as a response
-        if(result != null){
-            var data = {};
-            data["user"] = result.recordset;
-            res.send(data);
-        }else{ res.send("\nNo data to show!"); }
-        
-            
-    });
-});
+app.get('/Account', function (req, res) {
+  var request = new db.Request()
 
-var server = app.listen(5000, function () 
-{
-    console.log('Server is running..');
-});
+  // query to the database and get the records
+  request.query('select * from Account', function (err, result) {
+    if (err) { console.log(err); res.send(err.message) }
+
+    // send records as a response
+    if (result != null) {
+      var data = {}
+      data['user'] = result.recordset
+      res.send(data)
+    } else { res.send('\nNo data to show!') }
+  })
+})
+
+var server = app.listen(5000, function () {
+  console.log('Server is running..')
+})
--- db.js
+++ db.js
@@ -1,15 +1,14 @@
-const sql = require('mssql');
-
+const sql = require('mssql')
 
 var config = {
-    user: 'CityForumDBAdmin',
-    password: 'cloudService123',
-    server: 'cityforumdatabase.c7xajpqu6x8l.eu-west-1.rds.amazonaws.com', 
-    database: 'cityforumDB'
-};
+  user: 'CityForumDBAdmin',
+  password: 'cloudService123',
+  server: 'cityforumdatabase.c7xajpqu6x8l.eu-west-1.rds.amazonaws.com',
+  database: 'cityforumDB'
+}
 
-var connection = sql.connect(config, function(err){
-    if (err) console.log(err)
-});
+var connection = sql.connect(config, function (err) {
+  if (err) console.log(err)
+})
 
-module.exports = sql;
+module.exports = sql

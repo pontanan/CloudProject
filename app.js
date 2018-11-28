69a5597ff7e7451eae209d7af7edc4fcf0c343cb
Ebba Svensson <sveb1500@student.ju.se>
2018-11-28 15:41:10 +0100

    added regex to Post /posts

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/posts', function(request,response){
             else response.status(200).json(result);
         })
 });
-/*
+
 // GET /posts/id
 // uri: id = 3
 app.get('/posts/:id',function(request,response){
@@ -25,23 +25,18 @@ app.get('/posts/:id',function(request,response){
     // get the id from the uri
     const id = parseInt(request.params.id);
 
-    sql.connect(config, function(error){
-        if(error) response.status(500).json(error);
+    var sqlRequest = new db.Request();
 
-        var sqlRequest = new sql.Request();
+    // set parameter for query
+    sqlRequest.input('Id', db.Int, id);
 
-        // set parameter for query
-        sqlRequest.input('Id',sql.Int, id);
-
-        sqlRequest.query("SELECT * FROM Post WHERE ID = @Id", function (error, result){
-            if(result.recordset[0] == null){
-                response.status(400).json('post does not exist');
-            }
-            else if(error) response.status(500).end();
-            else response.status(200).json(result.recordset[0]);         
-        })
-    });
-   
+    sqlRequest.query("SELECT * FROM Post WHERE ID = @Id", function (error, result){
+        if(result.recordset.length == 0){
+            response.status(400).json('post does not exist');
+        }
+        else if(error) response.status(500).end();
+        else response.status(200).json(result.recordset[0]);         
+    })
 }); 
 
 // POST /posts
@@ -49,24 +44,28 @@ app.get('/posts/:id',function(request,response){
 app.post('/posts', function(request,response){
 
     const content = request.body.content;
-    const like = request.body.likeCount;
+    var like = request.body.likeCount;
     const dislike = request.body.dislikeCount;
     const time = request.body.time;
     const uid = request.body.userid;
 
-    //check if content is not an empty string
+    // if content is not an empty string
     if(content == ""){ response.status(400).json('Content cant be an empty string'); return; }
+   // regex to check numbers
+    var reg = new RegExp('^[0-9]+$');
+    // if time is not a number
+    if(!(reg.test(time))) { response.status(400).json('Time is not a number'); return; }
+     // if like and dislike is empty or does not contain numbers, set their default to 0
+    if(!(reg.test(like))) like = 0
+    if(!(reg.test(dislike))) dislike = 0
 
-    sql.connect(config, function(error){
-        if(error) response.status(500).json(error);
+        var sqlRequest = new db.Request();
 
-        var sqlRequest = new sql.Request();
-
-        sqlRequest.input('Uid',sql.Int, uid);
-        sqlRequest.input('Content',sql.Text,content);
-        sqlRequest.input('Like',sql.Int,like);
-        sqlRequest.input('Dislike',sql.Int, dislike);
-        sqlRequest.input('Time',sql.Int, time);
+        sqlRequest.input('Uid',db.Int, uid);
+        sqlRequest.input('Content',db.Text,content);
+        sqlRequest.input('Like',db.Int,like);
+        sqlRequest.input('Dislike',db.Int, dislike);
+        sqlRequest.input('Time',db.Int, time);
   
         sqlRequest.query("INSERT INTO Post VALUES (@Content, @Like , @Dislike, @Time, @Uid); SELECT SCOPE_IDENTITY() as ID", function (error, result) {
         
@@ -76,10 +75,9 @@ app.post('/posts', function(request,response){
             else response.status(500)
         } 
         else response.status(201).json('Location: posts/' + result.recordset[0].ID);
-        });   
-    });        
+        });        
 });
-
+/*
 // DELETE /posts/:id
 // uri: id = 3
 app.delete('/posts/:id',function(request,response){

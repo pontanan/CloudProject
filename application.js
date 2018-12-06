9b1122855cd2ba521abb37b82dfb7ebeae515e72
Pontus Anderö <datapontus@hotmail.com>
2018-12-06 12:25:19 +0100

    More work o auth/authz

--- application.js
+++ application.js
@@ -16,6 +16,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const request = require('request')
 const { OAuth2Client } = require('google-auth-library')
+const session = require('express-session')
 
 var app = express()
 app.use(bodyParser.json())
@@ -28,21 +29,31 @@ const client_secret = authConfig.web.client_secret
 const redirect_uri = authConfig.web.redirect_uris
 const auth_uri = authConfig.web.auth_uri
 const token_uri = authConfig.web.token_uri
-var JWTSecret = 'whathasthis589ff'
+var JWTSecret = 'whathasthis589ff' //Might not be used
 
 
-/////////////////////////////////////////////////////////////////////////////////
-///CURRENT STATUS: FUNCTIONING -> WORKS AS FAR AS GETTING GOOGLE_USER_ID (SUB)///
-/////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+///CURRENT STATUS: FUNCTIONING -> WORKS AS FAR AS SAVING GOOGLE_USER_ID (SUB)///
+////////////////////////////////////////////////////////////////////////////////
 //Start with http://localhost:5000/login
 
 //Token Verification Client
 const client = new OAuth2Client(client_id)
 
+//Session
+app.use(session( { 
+  secret: 'testsecret', 
+  resave: true,
+  saveUninitialized: false,
+  cookie: { maxAge: 36000000 }
+}))
+
 //Authentication URL
 var authUrl = `${auth_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=openid`
 
-app.get('/', function (req, res) {
+app.get('/', isAuthenticated, function (req, res) {
+  if(req.session.authenticated = 1)
+    req.session.authenticated = null
   var request = new db.Request()
 
   // query to the database and get the records
@@ -62,7 +73,7 @@ app.get('/', function (req, res) {
   var hashedPassword = bcrypt.hashSync(req.body.password, 8)
 })*/
 
-//Login
+//Login -> FOR TESTING
 //Redirects to google authentication
 app.get('/createToken', function (req, res) {
 
@@ -98,6 +109,9 @@ app.get('/me', function(req, res) {
   });
 });
 
+//TODO: Make sure authentication runs before anything
+//app.all('*', authenticate)
+
 app.get('/login', function (req, res) {
 
   res.redirect(301, authUrl)
@@ -108,12 +122,10 @@ app.get('/login', function (req, res) {
 //gets authentication code from google
 app.get('/auth/google/callback', function (req, res) {
   var authCode = req.query.code
-  var id = 15; //TODO: Get a way to find the id
 
   tokenUrl = `${token_uri}?code=${authCode}&client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${redirect_uri}&grant_type=authorization_code`
 
-  var sqlRequest = new db.Request();
-
+  //Posting request settings
   var options = { method: 'POST',
     url: token_uri,
     headers: { 'content-type': 'application/json'},
@@ -125,34 +137,45 @@ app.get('/auth/google/callback', function (req, res) {
       redirect_uri: 'http://localhost:5000/auth/google/callback' },
     json: true }
 
+  //Posting auth_code to google to get a id_token
   request(options, function(error, response, body) {
     if (error) throw new Error(error);
 
+    req.session.id_token = body.id_token
+
+    //Verify legitimacy of id_token
     async function verify() {
       const ticket = await client.verifyIdToken({
         idToken: body.id_token,
         audience: client_id
       })
       const payload = ticket.getPayload()
-      const userid = payload['sub']
 
-      sqlRequest.input('Id', db.Int, id);
-      sqlRequest.input('Google_id', db.Text, userid)
+      req.session.google_id = payload['sub'] //Save id_token in session
+      console.log(req.session.google_id)
+      console.log(req.session.redirectUrl)
 
+      //If directly from /login, else from other uri as authentication
+      if(req.session.redirectUrl == null) { res.redirect('/') }
+      else { 
+        var url = req.session.redirectUrl
+        req.session.redirectUrl = null
+        console.log('testing')
 
-      //TODO: make so google_id can be saved
-      sqlRequest.query("UPDATE Account SET google_id = @Google_id WHERE ID = @Id ", function(error, result){
-        if(error) { res.status(500).json(error); return; }   
-        if(!result) { res.status(400).json('user does not exist !!'); return; }
-        if(result.rowsAffected == 0) {res.status(400).json('user does not exist'); return; }
-        res.status(200).redirect(301, '/Account')
-      });
+        req.session.authenticated = 1
+        res.redirect(url)
+      }
     }
     verify().catch(console.error)
   })
 })
 
-app.get('/Account', function (req, res) {
+//TODO: Make sure app runs through authentication before accessing
+app.get('/Account', isAuthenticated, function (req, res) {
+  console.log(req.session.authenticated)
+  console.log('In account')
+  console.log(req.session.authenticated)
+
   res.send('It worked!')
 })
 
@@ -160,3 +183,32 @@ app.get('/Account', function (req, res) {
 app.listen(5000, function () {
   console.log('Server Running...')
 })
+
+//TODO: Make use of function, make so it works
+function authenticate(req, res, next) {
+
+  //Check if we have already run through the authentication
+  if(req.session.authenticated = 1) {
+    return next()
+  } else {
+    //If google_id does not exist, else redirect to login for authentication
+    if(req.session.google_id == null) {
+      var err = new Error('You must be logged in to view this page.');
+      err.status = 401;
+      console.log(err)
+      return next(err);
+    } else {
+      var url = req.url
+      req.session.redirectUrl = url
+
+      res.redirect('/login')
+    }
+  }
+}
+
+function isAuthenticated(req, res, next) {
+  if(req.session.authenticated = 1){
+    req.session.authenticated = null
+    return next()
+  }
+}
